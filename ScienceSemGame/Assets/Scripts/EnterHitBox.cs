74d6d2e28541b3e95c061c0034dc7b0eb6211a97
HaroldHuynh <hhuynh1228@gmail.com>
2025-08-17 14:30:19 -0700

    Removed SceneManager from SceneTransition prefab and made it its own

--- ScienceSemGame/Assets/Scripts/EnterHitBox.cs
+++ ScienceSemGame/Assets/Scripts/EnterHitBox.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class EnterHitBox : MonoBehaviour
 {
     [SerializeField] private GameObject toolTip;
+    [SerializeField] private string nextLevelName;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.layer == 6)
         {
             toolTip.SetActive(true);
+            SceneController.instance.LoadScene(nextLevelName);
         }
     }
 
