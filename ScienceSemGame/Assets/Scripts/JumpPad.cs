420660571d4c1ce56ae6221cb29fef44926800d6
HaroldHuynh <hhuynh1228@gmail.com>
2025-12-18 21:58:45 -0800

    Added Jump pad sound

--- ScienceSemGame/Assets/Scripts/JumpPad.cs
+++ ScienceSemGame/Assets/Scripts/JumpPad.cs
@@ -2,16 +2,18 @@ using UnityEngine;
 
 public class JumpPad : MonoBehaviour
 {
- 
+
     [SerializeField] private float bounce = 20f;
+    [SerializeField] private AudioClip bounceSound;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-  
+
         if (collision.gameObject.layer == 6)
         {
 
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
+            SoundManager.instance.PlaySoundFXClip(bounceSound, transform, 1f);
         }
     }
 }
